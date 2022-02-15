86dd59394eb152f33b1f1c7c4e00a38e9c8f7f8d
Pedro Ibarguren Zorrilla <pibargurenz01@educantabria.es>
2022-02-15 10:43:43 +0100

    Modifico perfil

--- my-app/src/components/Perfil.jsx
+++ my-app/src/components/Perfil.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 class Perfil extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.nombreIntroducido;
     }
 
 
